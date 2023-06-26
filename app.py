d9cd2a027721a6d5b80e7bfc95b1eb3fcdb6556d
Kebezo <112317873+Kebezo@users.noreply.github.com>
2023-06-26 12:05:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_session import Session
 from flask_mail import Mail, Message
+from redmail import outlook
+
 
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'secret'
 app.config['SESSION_TYPE'] = 'filesystem'
 
-app.config['MAIL_SERVER'] = 'smtp-mail.outlook.com'
+app.config['MAIL_SERVER'] = 'smtp.office365.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'jo3592@student.uni-lj.si'
+app.config['MAIL_USERNAME'] = 'jernej.ozebek@gmail.com'
 app.config['MAIL_PASSWORD'] = '5ekest4N'
 mail: Mail = Mail(app)
 
 Session(app)
 
+outlook.username = 'jernej.ozebek@gmail.com'
+outlook.password = '5ekest4N'
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -31,11 +35,15 @@ def chat():
 
 @app.route('/send-email', methods=['POST'])
 def send_email():
-    msg = Message('Hello from Flask', sender='jo3592@student.uni-lj.si', recipients=['jo3592@student.uni-lj.si'])
+    msg = Message('Hello from Flask', sender='jernej.ozebek@gmail.com', recipients=['jernej.ozebek@gmail.com'])
     msg.body = 'This is a test email sent from Flask using Outlook as the email service provider.'
-    mail.send(msg)
-
+    #mail.send(msg)
+    outlook.send(
+        subject="Example email",
+        receivers=['jernej.ozebek@gmail.com'],
+        text="Hi, this is an email."
+    )
     return render_template('chat.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
